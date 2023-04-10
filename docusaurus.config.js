8ecb32f097d1631f53d152e0153f46c9d17fbdf6
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2023-04-10 12:16:37 +0530

    update 5

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/',
               },
             ],
           },
