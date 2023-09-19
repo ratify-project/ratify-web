bab3a5d3a6c6779c623207dfd4aff24642f3e71e
Feynman Zhou <feynmanzhou@microsoft.com>
2023-09-19 12:00:24 +0800

    fix broken links on edit button
    Signed-off-by: Feynman Zhou <feynmanzhou@microsoft.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/deislabs/ratify-web/',
+            'https://github.com/deislabs/ratify-web/blob/main',
           lastVersion: 'current',
           versions: {
             current: {
