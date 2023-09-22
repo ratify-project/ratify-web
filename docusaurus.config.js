773ffc385874094bed370dcada612375b0194394
Akash Singhal <akashsinghal@microsoft.com>
2023-09-22 22:57:30 +0000

    chore: add favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Ratify',
   // tagline: 'Dinosaurs are cool',
-  // favicon: 'img/favicon.ico',
+  favicon: 'favicon.ico',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
